08f2c774b5aa3f7e2a74165e7d60528abab439b8
RobinParkerDWP <robin.parker@engineering.digital.dwp.gov.uk>
2024-08-13 16:07:10 +0100

    PHN-1824 invoice handshake (Agent UI) WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,33 +5,33 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-$(document).ready(function () {
-  window.MOJFrontend.initAll()
-})
+// $(document).ready(function () {
+//   window.MOJFrontend.initAll()
+// })
 
 
-if (typeof MOJFrontend.MultiFileUpload !== "undefined") {
-  new MOJFrontend.MultiFileUpload({
-    container: $(".moj-multi-file-upload"),
-    uploadUrl: "/ajax-upload",
-    deleteUrl: "/ajax-delete",
-  });
-}
+// if (typeof MOJFrontend.MultiFileUpload !== "undefined") {
+//   new MOJFrontend.MultiFileUpload({
+//     container: $(".moj-multi-file-upload"),
+//     uploadUrl: "/ajax-upload",
+//     deleteUrl: "/ajax-delete",
+//   });
+// }
 
-new MOJFrontend.FilterToggleButton({
-  bigModeMediaQuery: "(min-width: 48.063em)",
-  startHidden: true,
-  toggleButton: {
-    container: $(".moj-action-bar__filter"),
-    showText: "Show filter",
-    hideText: "Hide filter",
-    classes: "govuk-button--secondary",
-  },
-  closeButton: {
-    container: $(".moj-filter__header-action"),
-    text: "Close",
-  },
-  filter: {
-    container: $(".moj-filter"),
-  },
-});
+// new MOJFrontend.FilterToggleButton({
+//   bigModeMediaQuery: "(min-width: 48.063em)",
+//   toggleButton: {
+//     container: $(".moj-action-bar__filter"),
+//     showText: "Show filter",
+//     hideText: "Hide filter",
+//     classes: "govuk-button--secondary",
+//   },
+//   closeButton: {
+//     container: $(".moj-filter__header-action"),
+//     text: "Close",
+//   },
+//   filter: {
+//     container: $(".moj-filter"),
+//   },
+// });
+
